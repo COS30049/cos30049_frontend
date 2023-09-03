279994e73ab19d21b68319bfb116c39746f57cac
Ryan <ryan.v050358@gmail.com>
2023-09-03 17:53:04 +1000

    Updates

---
+++ precache-manifest.3e806c102cc02a1381ad50892f015c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3abaeb799f25fe411547",
+    "url": "/cos30049_frontend/static/css/main.8f5f0724.chunk.css"
+  },
+  {
+    "revision": "3abaeb799f25fe411547",
+    "url": "/cos30049_frontend/static/js/main.c85a3f37.chunk.js"
+  },
+  {
+    "revision": "4f3a8ff93aec8b1d448f",
+    "url": "/cos30049_frontend/static/js/runtime~main.b65228a1.js"
+  },
+  {
+    "revision": "7255606bb988d30d35ce",
+    "url": "/cos30049_frontend/static/js/2.5fe7b5af.chunk.js"
+  },
+  {
+    "revision": "079eb6dd64d20ab1b633",
+    "url": "/cos30049_frontend/static/js/3.d4e18d1a.chunk.js"
+  },
+  {
+    "revision": "06ce03201c615b2f6f2305387e866a24",
+    "url": "/cos30049_frontend/static/media/logo.06ce0320.svg"
+  },
+  {
+    "revision": "c46689bf1cb7989d9876807ec45027df",
+    "url": "/cos30049_frontend/index.html"
+  }
+];
