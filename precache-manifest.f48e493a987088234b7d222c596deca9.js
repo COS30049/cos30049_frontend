255e14c31495c9b595f611a63bcb63422db33816
Minh Nguyen <minhnnguyen003@gmail.com>
2023-09-05 00:44:49 +1000

    Updates

--- precache-manifest.f48e493a987088234b7d222c596deca9.js
+++ precache-manifest.f48e493a987088234b7d222c596deca9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "6830fad17fc2649d493b",
+    "revision": "0e81ffe471f694284c42",
     "url": "/cos30049_frontend/static/css/main.5efa454d.chunk.css"
   },
   {
-    "revision": "6830fad17fc2649d493b",
-    "url": "/cos30049_frontend/static/js/main.7e513b69.chunk.js"
+    "revision": "0e81ffe471f694284c42",
+    "url": "/cos30049_frontend/static/js/main.4edb0149.chunk.js"
   },
   {
     "revision": "59d4c77f52c302ba8031",
     "url": "/cos30049_frontend/static/js/runtime~main.35351ee1.js"
   },
   {
-    "revision": "f63401090f357ffaa740",
-    "url": "/cos30049_frontend/static/js/2.3fea0e87.chunk.js"
+    "revision": "7e228595969c6da5ecfe",
+    "url": "/cos30049_frontend/static/js/2.68aefa10.chunk.js"
   },
   {
     "revision": "6fa8f78dc06a29e4d507",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/cos30049_frontend/static/media/logo.06ce0320.svg"
   },
   {
-    "revision": "541fcd04ffe0d37167946be0d6d687bb",
+    "revision": "ef28239cf2d579a8396d24b88e25b318",
     "url": "/cos30049_frontend/index.html"
   }
 ];
