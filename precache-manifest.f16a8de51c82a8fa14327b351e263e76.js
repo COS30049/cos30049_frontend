13b8e8a54522fdbdf3d2f18f4df5460ead39f432
Minh Nguyen <minhnnguyen003@gmail.com>
2023-09-05 22:53:03 +1000

    Updates

---
+++ precache-manifest.f16a8de51c82a8fa14327b351e263e76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76a3f3a2a88e4fcdddf",
+    "url": "/cos30049_frontend/static/css/main.52fd5471.chunk.css"
+  },
+  {
+    "revision": "d76a3f3a2a88e4fcdddf",
+    "url": "/cos30049_frontend/static/js/main.468908c5.chunk.js"
+  },
+  {
+    "revision": "05f30bc3271df74b0c97",
+    "url": "/cos30049_frontend/static/js/runtime~main.d021cb8d.js"
+  },
+  {
+    "revision": "7d13bc8d8dbff19ad265",
+    "url": "/cos30049_frontend/static/js/2.7b5f9bd4.chunk.js"
+  },
+  {
+    "revision": "ec408357a189279fe535",
+    "url": "/cos30049_frontend/static/js/3.d4ebfc68.chunk.js"
+  },
+  {
+    "revision": "06ce03201c615b2f6f2305387e866a24",
+    "url": "/cos30049_frontend/static/media/logo.06ce0320.svg"
+  },
+  {
+    "revision": "33fd9b653d5c9e0b1d78c40a2526f9e2",
+    "url": "/cos30049_frontend/index.html"
+  }
+];
