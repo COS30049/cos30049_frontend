818320d97d59a693b43b569a4377e13a64fe5e11
Hoang Bao Phuc Chau <103523966@student.swin.edu.au>
2023-10-18 14:10:46 +1100

    add Profile Component and Adjusted the router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { createTheme } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import PopUp from './components/PopUp';
 import Products from './components/Products';
+import Profile from './components/Profile';
 
 export default function App() {
   // this state being add for illustration use only (since no back-end work yet)
@@ -21,6 +22,7 @@ export default function App() {
                 <Routes>
                   <Route path='/' element={<Home />}/>
                   <Route path='/login' element={<PopUp />}/>
+                  <Route path='/profile' element={<Profile />}/>
                 </Routes>
               </Router>
               <div>
